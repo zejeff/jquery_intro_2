322eca15d199fa232fb68237b07add308373ed5c
Jeffry <jeffryfleischer@gmail.com>
2020-06-27 18:10:02 -0700

    add additional js function to hide content

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
+// This only parses once on click.  Each click will activate the function and hide then show the content.  
   $(".clickable").click(function() {
     $("#walrus-hidden").hide();
     $("#walrus-showing").show();
+  });  
+// To re-hide the content, add a function with a unique class/id and reverse the methods being called.  
+  $(".clickable_alt").click(function() {
+    $("#walrus-showing").hide();
+    $("#walrus-hidden").show();
   });
 });
